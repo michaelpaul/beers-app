4674b7d1dc7cb98c9b217a687579b18afed4f3df
Michael Paul <michael@michaelpaul.com.br>
2018-11-12 17:53:44 +0000

    More retrival tests

--- tests/api/BeerCest.php
+++ tests/api/BeerCest.php
@@ -36,18 +36,46 @@ class BeerCest
         ]);
     }
 
+    public function overviewBeers(ApiTester $I)
+    {
+        $I->sendHEAD('/beers');
+        $I->seeResponseCodeIs(200);
+        $I->seeHttpHeader('Link', '<http://localhost/api/beers?page=1>; rel=self');
+        $I->seeHttpHeader('X-Pagination-Current-Page', 1);
+        $I->seeHttpHeader('X-Pagination-Page-Count', 1);
+        $I->seeHttpHeader('X-Pagination-Per-Page' , 20);
+        $I->seeHttpHeader('X-Pagination-Total-Count', 3);
+    }
+
     public function getBeers(ApiTester $I)
     {
         $I->sendGET('/beers');
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
+        $I->seeHttpHeader('Link', '<http://localhost/api/beers?page=1>; rel=self');
         $I->seeHttpHeader('X-Pagination-Current-Page', 1);
         $I->seeHttpHeader('X-Pagination-Page-Count', 1);
         $I->seeHttpHeader('X-Pagination-Per-Page' , 20);
         $I->seeHttpHeader('X-Pagination-Total-Count', 3);
-        $I->seeResponseContains('"id":');
-        $I->seeResponseContains('"name":');
-        $I->seeResponseContains('"abv":');
+        $I->seeResponseContainsJson([
+            [
+                'id' => 1,
+                'name' => 'Guinness',
+                'abv' => '4.20%',
+                'brewery_location' => 'Ireland, Dublin'
+            ],
+        ]);
+    }
+
+    public function getBeersInXML(ApiTester $I)
+    {
+        $I->haveHTTPHeader('Accept', 'application/xml');
+        $I->sendGET('/beers');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsXml();
+        $I->seeXmlResponseMatchesXpath('/response/item[1]');
+        $I->seeXmlResponseMatchesXpath('/response/item[2]');
+        $I->seeXmlResponseMatchesXpath('/response/item[3]/name[text()="Brahma"]');
     }
 
     public function getRandomBeer(ApiTester $I)
@@ -61,6 +89,19 @@ class BeerCest
             'description' => 'string',
             'abv' => 'string',
             'brewery_location' => 'string',
+        ]);
+    }
+
+    public function getBeer(ApiTester $I)
+    {
+        $I->sendGET('/beers/2');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'id' => 2,
+            'name' => 'Smithwick’s Irish Ale',
+            'abv' => '4.50%',
+            'brewery_location' => 'Ireland, Kilkenny'
         ]);
     }
 }
--- todo.md
+++ todo.md
@@ -1,5 +1,6 @@
 ok - fixtures: mover elas das migrações para fixtures que posso usar nos testes e no site em dev
 - merge web and test configs
 - testes automaticos para todos os métodos da API
+ok -  testar resposta em XML
 - adicionar as configs do guia
 - acceptance test local e Travis
